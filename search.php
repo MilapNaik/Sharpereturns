c7d666f07e5696b1be2d0d66f01a98468a38ea96
Milap Naik <milapnaik@gmail.com>
2014-11-13 06:54:27 +0000

    query tweaking3

--- search.php
+++ search.php
@@ -37,7 +37,7 @@
 		
 		$article = $_GET["article"];
 		
-		$select="Select * FROM articles WHERE title='$article'";
+		$select="Select content FROM articles WHERE title='$article'";
 		if (!mysqli_query($con,$select)) {
   			die('Error: ' . mysqli_error($con));
 		}
@@ -45,7 +45,7 @@
 			$result = mysqli_query($con, $select);
 			$row = mysqli_fetch_array($result);
 			
-			echo $row['article']; 
+			echo $row['content']; 
 			
 		}
 		
