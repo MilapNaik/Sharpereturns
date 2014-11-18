ba4517675e6fd6edee825247ec23e25fe83f9b33
Milap Naik <milapnaik@gmail.com>
2014-11-18 05:42:50 +0000

    Fixed db connection

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 define("DBNAME", "milap_articles");
 define("DBUSER", "milap");
 define("DBPASS", "test");
-define("DBHOST", "99webs.org");
+define("DBHOST", "localhost");
 $dbC = mysqli_connect(DBHOST, DBUSER, DBPASS, DBNAME)
         or die('Failed to Connect to Database');
 
